dbe3178d5854eeac0c8e7f180fb99b0d463e4974
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-04-26 08:21:32 +0700

    Add project files.

---
+++ GrassHook.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.TerrainFeatures;
+using System.Reflection;
+
+namespace BetterMotion;
+
+[HarmonyPatch]
+static class GrassHook
+{
+    static BindingFlags PrivateStaticFlag = BindingFlags.NonPublic | BindingFlags.Static;
+    static BindingFlags PrivateObjFlag = BindingFlags.NonPublic | BindingFlags.Instance;
+    public static void Init(Harmony h)
+    {
+        var drawMethod = typeof(Grass).GetMethod(nameof(Grass.draw));
+        var prefixMethod = typeof(GrassHook).GetMethod(nameof(Draw), PrivateStaticFlag);
+        h.Patch(drawMethod, prefix: new(prefixMethod));
+    }
+    static FieldInfo whichWeedField = typeof(Grass).GetField("whichWeed", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo offset1Field = typeof(Grass).GetField("offset1", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo offset2Field = typeof(Grass).GetField("offset2", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo offset3Field = typeof(Grass).GetField("offset3", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo offset4Field = typeof(Grass).GetField("offset4", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo shakeRandomField = typeof(Grass).GetField("shakeRandom", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo flip_Field = typeof(Grass).GetField("flip", BindingFlags.Instance | BindingFlags.NonPublic);
+    static FieldInfo shakeRotation_Field = typeof(Grass).GetField("shakeRotation", BindingFlags.Instance | BindingFlags.NonPublic);
+
+
+    static bool Draw(Grass __instance, SpriteBatch spriteBatch)
+    {
+        var tileLocation = __instance.Tile;
+        int numberOfWeeds = __instance.numberOfWeeds;
+        int[] whichWeed = whichWeedField.GetValue(__instance) as int[];
+        int[] offset1 = offset1Field.GetValue(__instance) as int[];
+        int[] offset2 = offset2Field.GetValue(__instance) as int[];
+        int[] offset3 = offset3Field.GetValue(__instance) as int[];
+        int[] offset4 = offset4Field.GetValue(__instance) as int[];
+        bool[] flip = flip_Field.GetValue(__instance) as bool[];
+        var texture = __instance.texture;
+        float shakeRotation = (float)shakeRotation_Field.GetValue(__instance);
+        double[] shakeRandom = shakeRandomField.GetValue(__instance) as double[];
+
+        for (int i = 0; i < numberOfWeeds; i++)
+        {
+            Vector2 pos = (i != 4) ? (tileLocation * 64f + new Vector2((float)(i % 2 * 64 / 2 + offset3[i] * 4 - 4) + 30f, i / 2 * 64 / 2 + offset4[i] * 4 + 40)) : (tileLocation * 64f + new Vector2((float)(16 + offset1[i] * 4 - 4) + 30f, 16 + offset2[i] * 4 + 40));
+            spriteBatch.Draw(texture.Value, Game1.GlobalToLocal(Game1.viewport, pos),
+                new Rectangle(whichWeed[i] * 15, __instance.grassSourceOffset, 15, 20), Color.White, shakeRotation / (float)(shakeRandom[i] + 1.0), new Vector2(7.5f, 17.5f), 4f, flip[i] ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (pos.Y + 16f - 20f) / 10000f + pos.X / 10000000f);
+        }
+
+        return false;
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,115 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.TerrainFeatures;
+using System.Reflection;
+namespace BetterMotion;
+
+public sealed class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        Helper.Events.Display.RenderedWorld += Display_RenderedWorld;
+        Helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
+        var h = new Harmony(this.ModManifest.UniqueID);
+        h.PatchAll();
+        GrassHook.Init(h);
+    }
+
+    static FieldInfo shakeRotationField = typeof(Grass).GetField("shakeRotation", BindingFlags.NonPublic | BindingFlags.Instance);
+    static FastNoiseLite noise
+    {
+        get
+        {
+            if (_noise == null)
+            {
+                _noise = new();
+                _noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
+                noise.SetFractalOctaves(3);
+            }
+            return _noise;
+        }
+    }
+    static FastNoiseLite _noise = null;
+    static TerrainFeature[] Grasses;
+    static Vector2 HalfVec2 = new Vector2(0.5f, 0.5f);
+    private void GameLoop_UpdateTicked(object? sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
+    {
+        var player = Game1.player;
+        if (player == null)
+            return;
+
+        var map = player.currentLocation;
+        if (map == null)
+            return;
+        //delayShake = 5.0;
+        var time = Game1.ticks;
+        Grasses = map.terrainFeatures.Values.Where(f => f as Grass != null).ToArray();
+        noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
+        noise.SetFractalType(FastNoiseLite.FractalType.FBm);
+        noise.SetFractalOctaves(3);
+        noise.SetFractalLacunarity(.5f);
+        noise.SetFractalGain(0.2f);
+
+        foreach (Grass grass in Grasses)
+        {
+            var tile = grass.Tile;
+            var playerTile = player.Position;
+            playerTile.X /= 64f;
+            playerTile.Y /= 64f;
+
+            var tileCenter = grass.Tile + HalfVec2;
+            var playerTileCenter = playerTile + HalfVec2;
+
+
+            var offset = playerTileCenter - tileCenter;
+            var offsetLen = offset.Length();
+
+            float posScale = 20f;
+            float x = tile.X;
+            float y = tile.Y;
+            x *= posScale;
+            y *= posScale;
+
+            float speedMultiply = 1.0f;
+            float wildDir = -1f;
+            x += speedMultiply * time * -wildDir;
+            y += speedMultiply * time;
+
+
+            float rot = 0;
+            rot += noise.GetNoise(x, y) * 0.2f;
+            rot += noise.GetNoise(x * .2f, y * .2f) * 0.7f;
+            rot /= 0.7f + 0.2f;
+
+            if (offsetLen <= 1)
+            {
+                float distort = 0;
+                if (offset.X >= 0)
+                    distort -= 1 - offset.X;
+                else
+                    distort += 1 + offset.X;
+
+                distort = distort * (1 - offsetLen);
+                rot += distort;
+                rot = Math.Clamp(rot, -1, 1);
+            }
+
+            shakeRotationField.SetValue(grass, rot);
+        }
+    }
+
+    private void Display_RenderedWorld(object? sender, StardewModdingAPI.Events.RenderedWorldEventArgs e)
+    {
+        var b = e.SpriteBatch;
+        var player = Game1.player;
+        if (player == null)
+            return;
+
+        //var drawPos = Game1.GlobalToLocal(Game1.viewport, player.Position);
+        //b.Draw(Game1.cropSpriteSheet,
+        //    new Rectangle((int)drawPos.X, (int)drawPos.Y, 8, 8),
+        //    new Rectangle(104, 305, 1, 1), Color.White);
+    }
+}
